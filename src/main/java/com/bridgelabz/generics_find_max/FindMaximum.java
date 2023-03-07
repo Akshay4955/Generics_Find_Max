89b560a02b277b0f20b5d0df30c5c013b4ed4393
Akshay4955 <akshayshedge11@gmail.com>
2023-03-07 19:22:00 +0530

    Added: Code to find string maximum

--- src/main/java/com/bridgelabz/generics_find_max/FindMaximum.java
+++ src/main/java/com/bridgelabz/generics_find_max/FindMaximum.java
@@ -4,8 +4,10 @@ public class FindMaximum {
     public static void main(String[] args) {
         Integer numberOne = 12, numberTwo = 15, numberThree = 21;
         Float numberOneFloat = 12.5f, numberTwoFloat = 15.45f, numberThreeFloat = 21.65f;
+        String stringOne = "Apple", stringTwo = "Mango", stringThree = "Pineapple";
         System.out.println("Maximum is " + new FindMaximum().getMaximum(numberOne, numberTwo, numberThree));
         System.out.println("Maximum is " + new FindMaximum().getMaximum(numberOneFloat, numberTwoFloat, numberThreeFloat));
+        System.out.println("Maximum is " + new FindMaximum().getMaximum(stringOne, stringTwo, stringThree));
     }
 
     public Integer getMaximum(Integer numberOne, Integer numberTwo, Integer numberThree) {
@@ -23,6 +25,15 @@ public class FindMaximum {
             maximum = numberTwo;
         if (numberThree.compareTo(maximum) > 0)
             maximum = numberThree;
+        return maximum;
+    }
+
+    public String getMaximum(String stringOne, String stringTwo, String stringThree) {
+        String maximum = stringOne;
+        if (stringTwo.compareTo(maximum) > 0)
+            maximum = stringTwo;
+        if (stringThree.compareTo(maximum) > 0)
+            maximum = stringThree;
         return maximum;
     }
 }
