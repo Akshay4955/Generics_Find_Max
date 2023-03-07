5e7aab7fe085733433b43012e1ec66ba2f6b75ee
Akshay4955 <akshayshedge11@gmail.com>
2023-03-07 19:42:19 +0530

    Refactored: Code using generic class

--- src/main/java/com/bridgelabz/generics_find_max/FindMaximum.java
+++ src/main/java/com/bridgelabz/generics_find_max/FindMaximum.java
@@ -1,16 +1,24 @@
 package com.bridgelabz.generics_find_max;
 
-public class FindMaximum {
+public class FindMaximum < T extends Comparable> {
+
+    T valueOne, valueTwo, valueThree;
+
+    public FindMaximum (T valueOne, T valueTwo, T valueThree) {
+        this.valueOne = valueOne;
+        this.valueTwo = valueTwo;
+        this.valueThree = valueThree;
+    }
     public static void main(String[] args) {
         Integer numberOne = 12, numberTwo = 15, numberThree = 21;
         Float numberOneFloat = 12.5f, numberTwoFloat = 15.45f, numberThreeFloat = 21.65f;
         String stringOne = "Apple", stringTwo = "Mango", stringThree = "Pineapple";
-        System.out.println("Maximum is " + new FindMaximum().getMaximum(numberOne, numberTwo, numberThree));
-        System.out.println("Maximum is " + new FindMaximum().getMaximum(numberOneFloat, numberTwoFloat, numberThreeFloat));
-        System.out.println("Maximum is " + new FindMaximum().getMaximum(stringOne, stringTwo, stringThree));
+        System.out.println("Maximum is " + getMaximum(numberOne, numberTwo, numberThree));
+        System.out.println("Maximum is " + getMaximum(numberOneFloat, numberTwoFloat, numberThreeFloat));
+        System.out.println("Maximum is " + getMaximum(stringOne, stringTwo, stringThree));
     }
 
-    public < E extends Comparable > E getMaximum(E stringOne, E stringTwo, E stringThree) {
+    public static < E extends Comparable > E getMaximum(E stringOne, E stringTwo, E stringThree) {
         E maximum = stringOne;
         if (stringTwo.compareTo(maximum) > 0)
             maximum = stringTwo;
