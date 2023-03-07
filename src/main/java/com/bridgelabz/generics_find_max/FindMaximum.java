0a046910e7f55cc944512db290379beb8d890579
Akshay4955 <akshayshedge11@gmail.com>
2023-03-07 20:22:43 +0530

    Added: Code to get maximum from multiple parameters

--- src/main/java/com/bridgelabz/generics_find_max/FindMaximum.java
+++ src/main/java/com/bridgelabz/generics_find_max/FindMaximum.java
@@ -1,14 +1,11 @@
 package com.bridgelabz.generics_find_max;
 
-public class FindMaximum < T extends Comparable> {
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
-    T valueOne, valueTwo, valueThree;
+public class FindMaximum <T> {
 
-    public FindMaximum (T valueOne, T valueTwo, T valueThree) {
-        this.valueOne = valueOne;
-        this.valueTwo = valueTwo;
-        this.valueThree = valueThree;
-    }
     public static void main(String[] args) {
         Integer numberOne = 12, numberTwo = 15, numberThree = 21;
         Float numberOneFloat = 12.5f, numberTwoFloat = 15.45f, numberThreeFloat = 21.65f;
@@ -16,14 +13,21 @@ public class FindMaximum < T extends Comparable> {
         System.out.println("Maximum is " + getMaximum(numberOne, numberTwo, numberThree));
         System.out.println("Maximum is " + getMaximum(numberOneFloat, numberTwoFloat, numberThreeFloat));
         System.out.println("Maximum is " + getMaximum(stringOne, stringTwo, stringThree));
+        System.out.println("Maximum is " + getMaximumForMoreThanThree(2, 5, 8, 14, 18, 28, 32, 45, 48,56,34,87));
     }
 
-    public static < E extends Comparable > E getMaximum(E stringOne, E stringTwo, E stringThree) {
+    public static <E extends Comparable> E getMaximum(E stringOne, E stringTwo, E stringThree) {
         E maximum = stringOne;
         if (stringTwo.compareTo(maximum) > 0)
             maximum = stringTwo;
         if (stringThree.compareTo(maximum) > 0)
             maximum = stringThree;
         return maximum;
+    }
+
+    public static <T> T getMaximumForMoreThanThree(T valueOne, T... args) {
+//        List<T> listOfParameters = Arrays.asList(args);
+        Arrays.sort(args);
+        return args[args.length-1];
     }
 }
