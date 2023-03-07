fa51a51ee11e2653b42a289d3456bc23124cc87d
Akshay4955 <akshayshedge11@gmail.com>
2023-03-07 20:29:19 +0530

    Refactored: Code to print maximum in same method

--- src/main/java/com/bridgelabz/generics_find_max/FindMaximum.java
+++ src/main/java/com/bridgelabz/generics_find_max/FindMaximum.java
@@ -1,8 +1,6 @@
 package com.bridgelabz.generics_find_max;
 
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
 
 public class FindMaximum <T> {
 
@@ -10,24 +8,23 @@ public class FindMaximum <T> {
         Integer numberOne = 12, numberTwo = 15, numberThree = 21;
         Float numberOneFloat = 12.5f, numberTwoFloat = 15.45f, numberThreeFloat = 21.65f;
         String stringOne = "Apple", stringTwo = "Mango", stringThree = "Pineapple";
-        System.out.println("Maximum is " + getMaximum(numberOne, numberTwo, numberThree));
-        System.out.println("Maximum is " + getMaximum(numberOneFloat, numberTwoFloat, numberThreeFloat));
-        System.out.println("Maximum is " + getMaximum(stringOne, stringTwo, stringThree));
-        System.out.println("Maximum is " + getMaximumForMoreThanThree(2, 5, 8, 14, 18, 28, 32, 45, 48,56,34,87));
+        getMaximum(numberOne, numberTwo, numberThree);
+        getMaximum(numberOneFloat, numberTwoFloat, numberThreeFloat);
+        getMaximum(stringOne, stringTwo, stringThree);
+        getMaximumForMoreThanThree(2, 5, 8, 14, 18, 28, 32, 45, 48,56,34,87);
     }
 
-    public static <E extends Comparable> E getMaximum(E stringOne, E stringTwo, E stringThree) {
+    public static <E extends Comparable> void getMaximum(E stringOne, E stringTwo, E stringThree) {
         E maximum = stringOne;
         if (stringTwo.compareTo(maximum) > 0)
             maximum = stringTwo;
         if (stringThree.compareTo(maximum) > 0)
             maximum = stringThree;
-        return maximum;
+        System.out.println("Maximum is " + maximum);
     }
 
-    public static <T> T getMaximumForMoreThanThree(T valueOne, T... args) {
-//        List<T> listOfParameters = Arrays.asList(args);
+    public static <T> void getMaximumForMoreThanThree(T valueOne, T... args) {
         Arrays.sort(args);
-        return args[args.length-1];
+        System.out.println("Maximum is " + args[args.length - 1]);
     }
 }
